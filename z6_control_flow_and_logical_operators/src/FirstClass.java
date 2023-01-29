61a821b28b4236b2577eccccf12c43212efad53b
10003217 <Dawid.Galeziewski@ergo.com>
2023-01-29 19:35:33 +0100

    saving notes

--- z6_control_flow_and_logical_operators/src/FirstClass.java
+++ z6_control_flow_and_logical_operators/src/FirstClass.java
@@ -1,5 +1,5 @@
-public class FirstClass {
-    public static void main(String[] args) {
+public class FirstClass { // public is  Access Modifier. Allows us to define what part of the code can access certain element
+    public static void main(String[] args) { // this is a main method. Main method is something java looks for when first running the program. This is a entry point of our app
         System.out.print("Test from FirstClass");
     }
 }
