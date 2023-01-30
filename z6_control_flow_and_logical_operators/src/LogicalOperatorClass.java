ec74a4dff251f7ee010c3cffaeb5f2bc4ec713c5
10003217 <Dawid.Galeziewski@ergo.com>
2023-01-30 22:23:39 +0100

    saving notes

---
+++ z6_control_flow_and_logical_operators/src/LogicalOperatorClass.java
@@ -0,0 +1,8 @@
+public class LogicalOperatorClass {
+    public static void main(String[] args) {
+        //java has same logical operator as javascript
+        if((15 < 2) && (5 >1) || (0 == 0)){ 
+            System.out.println("howdy");
+        }
+    }
+}
