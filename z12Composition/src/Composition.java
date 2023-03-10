60aa17f6ea736c2cd2fd29fb1ff93d8376a5ce4e
10003217 <Dawid.Galeziewski@ergo.com>
2023-03-10 16:09:29 +0100

    save notes

--- z12Composition/src/Composition.java
+++ z12Composition/src/Composition.java
@@ -1,8 +1,26 @@
 public class Composition {
     // inheritance defines an "IS A" relationship. I.E. monitor is a product, cat is a animal
     // Composition defines a HAS A relationship
+    // as General rule we probably want to try use "composition before inheritance"
 
-    // When using composition we create a object out of different parts that make a whole
+    // composition is better than inheritance due to fact that it is more flexible, we can add or remove parts and they will less likely have downstream effect
+    // inheritacne will break encapsulation, sublclasses may need direct access to a parents state or behaviour
+
+    public static void main(String[] args) {
+        // When using composition we create a object out of different parts that make a whole
+        ComputerCase exCaseTerminator = new ComputerCase("t12", "Honda");
+        Motherboard motherFBoarder = new Motherboard("i12", "Dell");
+        Monitor gawker = new Monitor("watcher12", "Samsung");
+
+
+        PersonalComputer beastPC = new PersonalComputer("beast", "mixed", exCaseTerminator, gawker, motherFBoarder);
+        // composition: creating a pc object, composed out of own custom classes
+        // We do not want to allow usage of those components directly
+        //  beastPC.getMotherboard().loadProgram("MS word");
+        //  beastPC.getMonitor().drawPixelAt(2,2, "red");
+
+        beastPC.powerUp(); // composition is mostly about creating objects inside the object
+    }
 
 }
 
@@ -20,28 +38,59 @@ class PersonalComputer extends Product { // we firstly want to extand same Produ
         this.motherboard = motherboard;
     }
 
-    public ComputerCase getComputerCase() {
-        return computerCase;
-    }
+    private void drawLogo(){ // we make this private as we only want it to be run from inside our class (other methods of this class)
+        this.monitor.drawPixelAt(1200,5, "yellow"); // we abstract the method away, use the components in our class methods
+    };
 
-    public Monitor getMonitor() {
-        return monitor;
+    public  void powerUp(){
+        this.computerCase.pressPowerButton();
+        this.drawLogo();
     }
+//    public ComputerCase getComputerCase() {
+//        return computerCase;
+//    }
+//
+//    public Monitor getMonitor() {
+//        return monitor;
+//    }
+//
+//    public Motherboard getMotherboard() {
+//        return motherboard;
+//    }
+}
 
-    public Motherboard getMotherboard() {
-        return motherboard;
+class Dimensions {
+    private int width;
+    private int height;
+    private int depth;
+
+    Dimensions(int width, int height, int depth){
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
     }
 }
 
 class Product {
     private String manufacturer;
     private String model;
-    private int width;
-    private int height;
-    private int depth;
+
+
+    // a example of using composition over inheritance is: we may have a digital product, this wont have those properties. We can modify this class by moving those properties into new object callsed Dimensions
+//    private int width;
+//    private int height;
+//    private int depth;
+
+    private Dimensions dimensions; // moved properties we do not want to be inherited by every product into composition
+
     public Product(String model, String manufacturer){
         this.model = model;
         this.manufacturer = manufacturer;
+    }
+
+    public Product(String model, String manufacturer, Dimensions dimensions){
+        this(model, manufacturer);
+        this.dimensions = dimensions;
     }
 }
 
