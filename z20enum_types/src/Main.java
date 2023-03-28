b763d2609b5e84d9a12359b00ddd927d8ccd41f8
10003217 <Dawid.Galeziewski@ergo.com>
2023-03-28 15:42:17 +0200

    saving notes

---
+++ z20enum_types/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // enum describes a collection of constants
+        // enums are like array but are always known, not changable and each element can be referred to by constant name and not a index
+        var day1 = Days.MONDAY;
+        System.out.printf("value of enum is %s, its name is %s, its ordinal is %s %n", day1, day1.name(), day1.ordinal());
+
+        // if we want to get a item by index
+        var allDays = Days.values(); // converts into array of values
+        System.out.printf("by index: %s%n", allDays[2]);
+    }
+
+    // enam is described similary to class. Convention is to start it with uppercase letter like a class.
+    public enum Days {
+        MONDAY, TUESDAY, WEDNESDAY //labels
+    }
+}
