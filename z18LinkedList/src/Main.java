b7d2843aa78ed80055678d339f6b1b73a285901c
10003217 <Dawid.Galeziewski@ergo.com>
2023-03-28 12:43:19 +0200

    saving notes

--- z18LinkedList/src/Main.java
+++ z18LinkedList/src/Main.java
@@ -1,14 +1,13 @@
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         big0();
         LinkedLists();
         iterators();
+        listIterator();
+        understandingCursorPositions();
     };
 
         static void big0(){
@@ -83,10 +82,54 @@ public class Main {
             // we can treverse lists either by using for loops or iterators
             // iterators work in similar fashion database cursors work (fetch prat or larget set of data, iterate thru it going one row at a time)
             // instance of a iterator will have a next method
+            // It is important to understand that iterator cursor positioons are BETWEEN the elements
+            // [{0 cursor position} El1, {1 cursor position}EL2, {2 cursor position}EL3, {3 cursor position}EL4]
 
             String[] shoppingArray = {"Carrots", "Onions", "Salt", "Pepper"};
-            List<String> shoppingList = List.of(shoppingArray);
+            List<String> shoppingList = List.of(shoppingArray); // iterator method is also available on List
             LinkedList<String> shoppingLinkedList = new LinkedList<>(shoppingList);
 
+            Iterator<String> iterator = shoppingLinkedList.iterator();
+
+            while(iterator.hasNext()){// on each iteration we check if there is  something after the cursor
+                String item = iterator.next(); // we move cursor to the next item, returning that item
+                // where iterator comes in handy is when we want to modify the list while we iterate over it
+                if(item.equals("Salt")){
+                    // !Important mutating iterator will mutate the list
+                    // We should not remove the item from the list itself during the iterations, this will throw a concurrency error
+                    iterator.remove(); // we remove this item from iterator. This will also remove the item from the list iterator does referance
+                }
+                System.out.println(item);
+            }
+            System.out.printf("iterator is: %s;%n list is: %s%n", iterator, shoppingLinkedList);
+        }
+
+        static void listIterator(){
+            // unlike list iterator, list iterator can go both forward and backwords, it supports add and set methods as well
+            LinkedList<String> buildingMaterials = new LinkedList<>(List.of("bricks", "cement", "planks", "nails"));
+            ListIterator<String> iterator = buildingMaterials.listIterator(1); // by passing index we start from another position then index 0
+
+            while(iterator.hasNext()){
+                if(iterator.next().equals("cement")){
+                    iterator.add("brick"); // allows us to add items as well
+                }
+            }
+
+            // we can go back as well
+            while(iterator.hasPrevious()){
+                System.out.println(iterator.previous());
+            }
+
+            System.out.println(buildingMaterials);
+        }
+
+        static void understandingCursorPositions(){
+            LinkedList<Integer> trainStations = new LinkedList<>(List.of(0,1,2,3,4));
+            ListIterator<Integer> iterator = trainStations.listIterator();
+            // IMPORTANT to understand is that iterator cursor is never at the point of our item in the collection. It is before it, and after it PASSES it it will return its value
+            System.out.printf("Passing station: %s%n", iterator.next()); // was before index 0, now is before index 1
+            System.out.printf("Passing station: %s%n", iterator.next());
+            System.out.printf("Passing station: %s%n", iterator.next()); // now it is after index 2. It passed this index so it is returning its value
+            System.out.printf("Passing station: %s%n", iterator.previous()); // we are going back but we are passing index 2 again so it will be returned the second time
         }
 }
