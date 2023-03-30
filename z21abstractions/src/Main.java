ab8a6c54e79468ee5d54dc04ae0ebe5fde75679c
10003217 <Dawid.Galeziewski@ergo.com>
2023-03-30 09:48:43 +0200

    save notes

--- z21abstractions/src/Main.java
+++ z21abstractions/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // abstraction and generalisation are concepts that support extensible and flexible code.
@@ -12,18 +14,70 @@ public class Main {
         // There are also abstract methods: abstract methods have a method signature, and a return type, but do not have a method body
         // Abstract methods are a type of contract, promising that all subtypes will provide promised functionality
         // anthysesis to abstract methods are concrete methods
+
+        Dog dog = new Dog("dog", "big", 30);
+        Cat cat = new Cat("cat", "big", 30);
+
+        dog.makeNoise();
+        cat.makeNoise();
+
+        //as with normal inheritance we can use child classes where parent class was used
+        doAnimalStuff(dog);
+        doAnimalStuff(cat);
+        List<Animal> zoo = List.of(new Dog("dog", "big", 30), new Cat("cat", "big", 30)); // same goes for collections. We can use a child class and use methods that it has in common with the parent
     }
 
-    abstract class Animal { // abstract class modifier
-        // abstract class is incomplete, therefore we cannot create a instance of abstract class using the "new" keyword
+    public static void doAnimalStuff(Animal animal){
+        animal.move(300);
+    }
+}
 
+
+
+abstract class Animal { // abstract class modifier
+    // abstract class is incomplete, therefore we cannot create a instance of abstract class using the "new" keyword
+
+    public Animal(String type, String size, double weight){
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+    protected String type;
+    private String size;
+    private double weight;
+    // when using abstrac class with abstract methods a subclass HAS to implement those methods
+    // also subclass won't be able to inherit a abstract methods
+    public abstract void move(int speed); // abstract method has no body, just the class signature. This will force implementation of this method on each concrete class extending this one
+
+    protected void makeNoise(){
+        System.out.printf("% is making a noise %s%n", this.type); //this however will work just like normal method when inheriting
+    }
+}
+
+abstract class Mammal extends Animal { // one abstract class can extend another
+    public Mammal(String type, String size, double weight){
+        super(type, size, weight); // abstract class will force our subclasses to call a super constructor, despite the fact the abstract class cannot be initialized on its own
+    }
+}
+
+class Dog extends Animal { // Mammal is abstract class but dog is concrete
+    public Dog(String type, String size, double weight){
+        super(type, size, weight);
     }
 
-    abstract class Mammal extends Animal { // one abstract class can extend another
+    @Override
+    public void move(int speed){
+        System.out.printf("Dog running with speed %s%n", speed);
+    }
+}
 
+class Cat extends Animal { // Mammal is abstract class but dog is concrete
+    public Cat(String type, String size, double weight){
+        super(type, size, weight);
     }
 
-    class Dog extends Animal { // Mammal is abstract class but dog is concrete
-
+    @Override
+    public void move(int speed){
+        System.out.printf("Cat running with speed %s%n", speed);
     }
 }
